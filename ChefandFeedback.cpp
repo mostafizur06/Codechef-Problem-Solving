7c9347b28c8346ae958b1161b1487d7423414721
Md. Mostafizur Rahman <93403578+mostafizur06@users.noreply.github.com>
2023-07-03 21:45:13 +0600

    Chef and Feedback

--- ChefandFeedback.cpp
+++ ChefandFeedback.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        string S, s1, s2;
+        cin >> S;
+        s1 = "010";
+        s2 = "101";
+        int c = S.find(s1); //// if find the return 0 if not then return -1
+        int d = S.find(s2);
+        if (c == 0 || d == 0)
+        {
+            cout << "Good" << endl;
+        }
+        else
+            cout << "Bad" << endl;
+    }
+}
